ceb34893586e66fdb493ca5b2c6cc29cb80178d8
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-01 11:54:20 -0600

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -5,8 +5,8 @@ forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
 forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
 forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
 forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
-forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
-forma.faces.push( new THREE.Face3( 0, 2, 1 ) ); // Cara 0
+forma.vertices.push( new THREE.Vector3( 0,  3,  0 ) ); // Vértice 4
+forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
 forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
 forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
 forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3

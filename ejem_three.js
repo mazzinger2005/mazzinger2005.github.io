445474265a4ed8e3cbdc13d4d07e791b79ceb861
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-02-28 18:28:35 -0600

    Update ejem_three.js

--- ejem_three.js
+++ ejem_three.js
@@ -7,7 +7,7 @@ renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
 document.body.appendChild( renderer.domElement );
 renderer.render( escena, camara );
 
-var forma = new THREE.BoxGeometry( 1, 2, 1 );
+var forma = new THREE.BoxGeometry( 0, 2, 1 );
 var cubo  = new THREE.Mesh( forma );
 cubo.rotation.x += 0.1;
 cubo.rotation.y += 0.1;

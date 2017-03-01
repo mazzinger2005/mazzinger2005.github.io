95bc73f3bf1fb1985b4afacc13c08791dc5d63fe
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-01 16:22:32 -0600

    Update bandoblanco.js

--- bandoblanco.js
+++ bandoblanco.js
@@ -4,9 +4,9 @@ forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
 forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
 forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
 forma.vertices.push( new THREE.Vector3( 0,  2,  0 ) ); // Vértice 4
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
+forma.faces.push( new THREE.Face3( 0, 2, 1 ) ); // Cara 0
+forma.faces.push( new THREE.Face3( 4, 1, 0 ) ); // Cara 1
+forma.faces.push( new THREE.Face3( 2, 0, 4 ) ); // Cara 2
 forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
 forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
 forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5

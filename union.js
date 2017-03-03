5cd7470cdd1641b7001293570a655e842946d2ca
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-03 10:51:06 -0600

    Update union.js

--- union.js
+++ union.js
@@ -1,13 +1,17 @@
 var troncoForma = new THREE.CylinderGeometry(1, 1, 1.5);
-var esferaForma = new THREE.SphereGeometry(.8);
+var cuboForma = new THREE.CubeGeometry(1, 0.5, 1);
+var esferaForma = new THREE.SphereGeometry(1);
 esferaForma.translate(0,1,0);
+cuboForma.translate(0,-1.5,0);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
+var cuboMalla = new THREE.Mesh(cuboForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
 
 var arbolForma = new THREE.Geometry();
 arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+arbolForma.merge(cuboMalla.geometry, cuboMalla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var arbolMalla = new THREE.Mesh(arbolForma, material);

daf038f9c4cf336fdec1dddf1b5ea47857ead8e0
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-03 11:31:16 -0600

    Update extrusion.js

--- extrusion.js
+++ extrusion.js
@@ -1,20 +1,30 @@
 var figura = new THREE.Shape();
 
 figura.moveTo(10, 10);
-figura.lineTo(10, 40);
-figura.lineTo(40, 40);
+figura.lineTo(0, 20);
+figura.lineTo(5, 23);
+figura.lineTo(0, 30);
+figura.lineTo(10, 26);
+figura.lineTo(20, 40);
+figura.lineTo(30, 26);
+figura.lineTo(40, 30);
+figura.lineTo(35, 23);
+figura.lineTo(40, 20);
+figura.lineTo(30, 10);
+figura.lineTo(20, 15);
 figura.lineTo(10, 10);
+
 var forma = new THREE.ExtrudeGeometry( figura,
-                                       {amount: 30} );
+                                       {amount: 20} );
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateY( Math.PI/4 );
+malla.rotateY( Math.PI/8 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 200;
+camara.position.z = 500;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,

39fb1d4c347e3e6194296b6732b184ac16409b41
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-02 19:23:58 -0600

    Update extrusion.js

--- extrusion.js
+++ extrusion.js
@@ -5,7 +5,7 @@ figura.lineTo(10, 40);
 figura.lineTo(40, 40);
 figura.lineTo(10, 10);
 var forma = new THREE.ExtrudeGeometry( figura,
-                                       {amount: 10} );
+                                       {amount: 20} );
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 malla.rotateY( Math.PI/4 );
@@ -14,7 +14,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 200;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,

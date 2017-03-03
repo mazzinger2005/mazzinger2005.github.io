d85e3225062de0d39e179987a87c4be137109683
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-03 08:52:27 -0600

    Update tablero.js

--- tablero.js
+++ tablero.js
@@ -1,1 +1,30 @@
+WIDTH = window.innerWidth;
+HEIGHT = window.innerHeight;
+var lienzo = new THREE.WebGLRenderer({antialias: true});
 
+lienzo.setSize(WIDTH,HEIGHT);
+
+
+var puntos = [];
+for ( var i = 0; i < 50; i ++ ) {
+    puntos.push( new THREE.Vector2(
+                     Math.sin( i * 0.5 ) * 15 + 50,
+                     ( i - 5 ) * 3 ) );
+}
+var forma = new THREE.LatheGeometry(puntos);
+
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/6 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );

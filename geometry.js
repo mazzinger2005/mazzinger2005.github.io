48084b1277df5dd182c54a6df25d447a0392d0c9
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-02 18:21:46 -0600

    Update geometry.js

--- geometry.js
+++ geometry.js
@@ -6,12 +6,17 @@ forma.vertices.push( new THREE.Vector3( 1.5,  0, -1.5 ) ); // Vértice 1
 forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
 forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
 forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
+forma.vertices.push( new THREE.Vector3( 0, -2,  0 ) ); // Vértice 5
 forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
 forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
 forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
 forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
 forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
 forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
+forma.faces.push( new THREE.Face3( 3, 0, 5 ) ); // Cara 6
+forma.faces.push( new THREE.Face3( 0, 1, 5 ) ); // Cara 7
+forma.faces.push( new THREE.Face3( 1, 2, 5 ) ); // Cara 8
+forma.faces.push( new THREE.Face3( 2, 3, 5 ) ); // Cara 9
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
 

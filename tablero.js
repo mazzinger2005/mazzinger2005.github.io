079846db153bdaf540ede41eccce6e058123839b
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-03 21:04:46 -0600

    Update tablero.js

--- tablero.js
+++ tablero.js
@@ -136,8 +136,8 @@ var troncoForma = new THREE.CylinderGeometry(25, 10, 75);
 var troncoForma1 = new THREE.CylinderGeometry(10, 25, 75);
 var cuboForma = new THREE.CubeGeometry(60,15, 60);
 var cuboForma1 = new THREE.CubeGeometry(60,15, 60);
-cuboForma.translate(0,-15,0);
-cuboForma1.translate(0,75,0);
+cuboForma.translate(0,-30,0);
+cuboForma1.translate(0,150,0);
 troncoForma1.translate(0,75,0);
 
 var troncoMalla = new THREE.Mesh(troncoForma);

635921cb265e21551cb71e6f328c20b65c4d261c
mazzinger2005 <mazzinger_2005@hotmail.com>
2017-03-23 14:08:00 -0600

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,11 +1,13 @@
 
-function setup(){
-	
-	var listener=function(){
+function listener(){
 	camara.aspect=window.innerWidth/window.innerHeight
 	camara.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth/window.innerHeight)
-	}
+}
+var capturarp=false
+
+function setup(){
+	
 	escena=new THREE.Scene();
 
 	camara=new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight,0.1,1000);
@@ -20,7 +22,7 @@ function setup(){
 
 	var tipoEvento='resize';
 	var capturar=false;
-	window.addEventListener(tipoEvento,listener,capturar);
+	window.addEventListener(tipoEvento,listener,capturarp);
 }
 
 function loop(){
@@ -36,5 +38,4 @@ loop();
 
 malla.rotation.x
 malla.rotation.y
-setup();
-loop();
+
